788f708cd6b399d8ffe1aeb357aa1c0b62f5f145
AiTaif7 <Ai.Taif.Alharbi@gmail.com>
2024-03-31 03:10:03 +0000

    CheackPoint2

--- my_rb1_ros/src/rotate_service.cpp
+++ my_rb1_ros/src/rotate_service.cpp
@@ -1,95 +1,61 @@
+#include "ros/ros.h"
+#include "my_rb1_ros/Rotate.h"
+#include "nav_msgs/Odometry.h"
+#include "geometry_msgs/Twist.h"
 #include <cmath>
-#include <geometry_msgs/Twist.h>
-#include <my_rb1_ros/Rotate.h>
-#include <nav_msgs/Odometry.h>
-#include <ros/ros.h>
-#include <tf/transform_datatypes.h>
 
-class MyRb1Server {
-protected:
-  ros::NodeHandle nh_;
-
-  ros::Publisher pub_vel_;
-  geometry_msgs::Twist cmd_vel_;
-  ros::Subscriber sub_odom_;
-
-  ros::ServiceServer my_rb1_server_;
-  my_rb1_ros::Rotate::Request req_;
-  my_rb1_ros::Rotate::Response res_;
-
+class RotateService
+{
 public:
-  MyRb1Server() {
-    pub_vel_ = nh_.advertise<geometry_msgs::Twist>("/cmd_vel", 1);
-    my_rb1_server_ = nh_.advertiseService("/rotate_robot",
-                                          &MyRb1Server::RotateServerCB, this);
-  }
-
-  ~MyRb1Server() {}
-
-  void rotate_clockwise() {
-    cmd_vel_.angular.z = 0.1;
-    pub_vel_.publish(cmd_vel_);
-  }
-
-  void rotate_counterclockwise() {
-    cmd_vel_.angular.z = -0.1;
-    pub_vel_.publish(cmd_vel_);
-  }
-
-  void stop_motion() {
-    cmd_vel_.angular.z = 0.0;
-    pub_vel_.publish(cmd_vel_);
-  }
-
-  void RotateCB(const nav_msgs::OdometryConstPtr &msg) {
-    tf::Quaternion q(msg->pose.pose.orientation.x, msg->pose.pose.orientation.y,
-                     msg->pose.pose.orientation.z,
-                     msg->pose.pose.orientation.w);
-    tf::Matrix3x3 m(q);
-    tfScalar roll, pitch, yaw;
-    m.getEulerYPR(yaw, pitch, roll);
-
-    ROS_INFO("\n yaw: %.1f ===> req: %d", yaw * 180 / M_PI, req_.degrees);
-    if (req_.degrees == 90) {
-      if (req_.degrees >= (yaw * 180 / M_PI)) {
-        rotate_counterclockwise();
-      } else {
-        res_.result = "SUCCESS";
-        stop_motion();
-        ROS_INFO("\n The robot have made a 90 deg counterclockwise spin");
-
-      }
-    } else {
-      if (req_.degrees <= (yaw * 180 / M_PI)) {
-        rotate_clockwise();
-      } else {
-        res_.result = "SUCCESS";
-        stop_motion();
-        ROS_INFO("\n The robot have made a 90 deg clockwise spin");
-      }
+    RotateService() : current_theta(0.0) {
+        serviceServer_ = nh_.advertiseService("/rotate_robot", &RotateService::handleRequest, this);
+        sub_ = nh_.subscribe("/odom", 1000, &RotateService::odomCallback, this);
+        pub_ = nh_.advertise<geometry_msgs::Twist>("/cmd_vel", 1);
+        ROS_INFO("Service server /rotate_robot ready.");
     }
-  }
 
-  bool RotateServerCB(my_rb1_ros::Rotate::Request &req,
-                      my_rb1_ros::Rotate::Response &res) {
-    req_ = req;
-    res_ = res;
-    sub_odom_ = nh_.subscribe("/odom", 10, &MyRb1Server::RotateCB, this);
-
-    ROS_INFO("The robot is spinning ...");
-    while (ros::ok() && res_.result != "SUCCESS") {
-      ros::spinOnce();
+    bool handleRequest(my_rb1_ros::Rotate::Request &req, my_rb1_ros::Rotate::Response &res) {
+        ROS_INFO("Rotating %d degrees.", req.degrees);
+        double radians = (req.degrees % 360) * (M_PI / 180.0); // Convert degrees to radians
+        double target_theta = fmod(current_theta + radians + 2 * M_PI, 2 * M_PI); // Calculate target in radians
+        ros::Rate rate(10);
+        while (ros::ok()) {
+            double orientation_error = target_theta - current_theta;
+            orientation_error = atan2(sin(orientation_error), cos(orientation_error)); // Normalize error
+            if (fabs(orientation_error) < 0.05) { // Check if within margin
+                cmd_.angular.z = 0.0; // Stop rotation
+                pub_.publish(cmd_);
+                ROS_INFO("Rotation completed successfully.");
+                res.result = "Rotation completed successfully";
+                return true;
+            }
+            cmd_.angular.z = (orientation_error > 0) ? 0.1 : -0.1;
+            pub_.publish(cmd_);
+            ros::spinOnce();
+            rate.sleep();
+        }
+        res.result = "Rotation NOT complete";
+        return false;
     }
-    sub_odom_.shutdown();
-    res.result = res_.result;
-    return true;
-  }
+
+    void odomCallback(const nav_msgs::Odometry::ConstPtr& msg) {
+        double siny_cosp = 2 * (msg->pose.pose.orientation.w * msg->pose.pose.orientation.z + msg->pose.pose.orientation.x * msg->pose.pose.orientation.y);
+        double cosy_cosp = 1 - 2 * (msg->pose.pose.orientation.y * msg->pose.pose.orientation.y + msg->pose.pose.orientation.z * msg->pose.pose.orientation.z);
+        current_theta = atan2(siny_cosp, cosy_cosp);
+    }
+
+private:
+    ros::NodeHandle nh_;
+    ros::ServiceServer serviceServer_;
+    ros::Publisher pub_;
+    ros::Subscriber sub_;
+    geometry_msgs::Twist cmd_;
+    double current_theta; // Track the current orientation in radians
 };
 
 int main(int argc, char **argv) {
-  ros::init(argc, argv, "rotate_server");
-
-  MyRb1Server my_rb1_server;
-
-  ros::spin();
+    ros::init(argc, argv, "rotate_service_server");
+    RotateService server;
+    ros::spin();
+    return 0;
 }
